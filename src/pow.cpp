e198de9593116a711a9e6966ea5b44343660f8de
kusano <kusano@users.noreply.github.com>
2018-06-27 05:23:17 +0900

    Implement Zawy's LWMA difficulty algorithm

--- src/pow.cpp
+++ src/pow.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2017 The Bitcoin Core developers
+// Copyright (c) 2018 The Kusacoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -9,66 +10,86 @@
 #include <chain.h>
 #include <primitives/block.h>
 #include <uint256.h>
+#include <algorithm>
 
 unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params& params)
 {
     assert(pindexLast != nullptr);
-    unsigned int nProofOfWorkLimit = UintToArith256(params.powLimit).GetCompact();
 
-    // Only change once per difficulty adjustment interval
-    if ((pindexLast->nHeight+1) % params.DifficultyAdjustmentInterval() != 0)
-    {
-        if (params.fPowAllowMinDifficultyBlocks)
-        {
-            // Special difficulty rule for testnet:
-            // If the new block's timestamp is more than 2* 10 minutes
-            // then allow mining of a min-difficulty block.
-            if (pblock->GetBlockTime() > pindexLast->GetBlockTime() + params.nPowTargetSpacing*2)
-                return nProofOfWorkLimit;
-            else
-            {
-                // Return the last non-special-min-difficulty-rules-block
-                const CBlockIndex* pindex = pindexLast;
-                while (pindex->pprev && pindex->nHeight % params.DifficultyAdjustmentInterval() != 0 && pindex->nBits == nProofOfWorkLimit)
-                    pindex = pindex->pprev;
-                return pindex->nBits;
-            }
-        }
-        return pindexLast->nBits;
-    }
-
-    // Go back by what we want to be 14 days worth of blocks
-    int nHeightFirst = pindexLast->nHeight - (params.DifficultyAdjustmentInterval()-1);
-    assert(nHeightFirst >= 0);
-    const CBlockIndex* pindexFirst = pindexLast->GetAncestor(nHeightFirst);
-    assert(pindexFirst);
-
-    return CalculateNextWorkRequired(pindexLast, pindexFirst->GetBlockTime(), params);
-}
-
-unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, int64_t nFirstBlockTime, const Consensus::Params& params)
-{
     if (params.fPowNoRetargeting)
         return pindexLast->nBits;
 
-    // Limit adjustment step
-    int64_t nActualTimespan = pindexLast->GetBlockTime() - nFirstBlockTime;
-    if (nActualTimespan < params.nPowTargetTimespan/4)
-        nActualTimespan = params.nPowTargetTimespan/4;
-    if (nActualTimespan > params.nPowTargetTimespan*4)
-        nActualTimespan = params.nPowTargetTimespan*4;
+    arith_uint256 powLimit = UintToArith256(params.powLimit);
 
-    // Retarget
-    const arith_uint256 bnPowLimit = UintToArith256(params.powLimit);
-    arith_uint256 bnNew;
-    bnNew.SetCompact(pindexLast->nBits);
-    bnNew *= nActualTimespan;
-    bnNew /= params.nPowTargetTimespan;
+    // Special difficulty rule for testnet:
+    // If the new block's timestamp is more than 2* 10 minutes
+    // then allow mining of a min-difficulty block.
+    if (params.fPowAllowMinDifficultyBlocks &&
+        pblock->GetBlockTime() > pindexLast->GetBlockTime() + params.nPowTargetSpacing*2)
+        return powLimit.GetCompact();
 
-    if (bnNew > bnPowLimit)
-        bnNew = bnPowLimit;
+    //  Zawy's LWMA (Linearly Weighted Moving Average)
+    //  https://github.com/zawy12/difficulty-algorithms/issues/3
+    //  https://github.com/BTCGPU/BTCGPU/blob/426289533623d88c0881cab74c265790fb665043/src/pow.cpp
+    const int height = pindexLast->nHeight + 1;
+    const int N = params.nZawyLwmaAveragingWindow;
+    const int64_t T = params.nPowTargetSpacing;
 
-    return bnNew.GetCompact();
+    int64_t solveTimeSum = 0;
+    int64_t solveTimeDen = 0;
+    arith_uint256 targetSum = 0;
+    int targetNum = 0;
+
+    int64_t solveTimeSum3 = 0;
+    int solveTimeNum3 = 0;
+    arith_uint256 targetPrev = 0;
+
+    for (int i=1; i<=N; i++)
+    {
+        //  Different from the original implementation, calculate target for initial blocks too.
+        if (height - N + i - 2 < 0)
+            continue;
+        const CBlockIndex *block = pindexLast->GetAncestor(height - N + i - 1);
+        const CBlockIndex *blockPrev = pindexLast->GetAncestor(height - N + i - 2);
+
+        int64_t solveTime = block->GetBlockTime() - blockPrev->GetBlockTime();
+        solveTime = std::max(-3*T, std::min(solveTime, 6*T));
+        solveTimeSum += i * solveTime;
+        solveTimeDen += i;
+
+        arith_uint256 target;
+        target.SetCompact(block->nBits);
+        //  Avoid overflow
+        //  If target become too small, care will be needed
+        target >>= 64;
+        targetSum += target;
+        targetNum++;
+
+        if (i > N-3)
+        {
+            solveTimeSum3 += solveTime;
+            solveTimeNum3++;
+        }
+        if (i == N)
+            targetPrev = target;
+    }
+    solveTimeSum = std::max(solveTimeSum, T*N*N/40);
+
+    arith_uint256 nextTarget = 0;
+    if (targetNum > 0)
+        //  next_target = (targetSum/targetNum) / (T / (solvetimeSum/solveTimeDen))
+        nextTarget = targetSum * solveTimeSum / targetNum / solveTimeDen / T;
+    else
+        nextTarget = powLimit>>64;
+
+    //  LWMA-2
+    if (solveTimeNum3 == 3 &&
+        solveTimeSum3 < T*8/10)
+        nextTarget = targetPrev*100/110;
+
+    nextTarget <<= 64;
+    nextTarget = std::min(nextTarget, UintToArith256(params.powLimit));
+    return nextTarget.GetCompact();
 }
 
 bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params& params)
