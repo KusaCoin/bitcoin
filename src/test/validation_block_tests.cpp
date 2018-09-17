0113ff8b712c373e5fe7bd9fa0b212a6a8da7131
kusano <kusano@users.noreply.github.com>
2018-09-18 05:30:13 +0900

    update validation_block_tests.cpp

--- src/test/validation_block_tests.cpp
+++ src/test/validation_block_tests.cpp
@@ -1,4 +1,5 @@
 // Copyright (c) 2018 The Bitcoin Core developers
+// Copyright (c) 2018 The Kusacoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -10,7 +11,7 @@
 #include <miner.h>
 #include <pow.h>
 #include <random.h>
-#include <test/test_bitcoin.h>
+#include <test/test_kusacoin.h>
 #include <validation.h>
 #include <validationinterface.h>
 
